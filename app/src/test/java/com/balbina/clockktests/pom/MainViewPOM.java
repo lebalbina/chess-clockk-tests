eb3106079f2a9a53666cf0f77a828256f1d36cd4
lebalbina <to.malwina.szyszka@gmail.com>
2025-10-15 10:27:40 +0200

    main view pom method based

--- app/src/test/java/com/balbina/clockktests/pom/MainViewPOM.java
+++ app/src/test/java/com/balbina/clockktests/pom/MainViewPOM.java
@@ -1,143 +1,130 @@
 package com.balbina.clockktests.pom;
 
+import com.balbina.clockktests.pom.utility.WaitHelper;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 //TODO  refactor to method-based lookup
 public class MainViewPOM {
 
+    private final WaitHelper helper;
+
+    private final By clockTopLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockTop\")");
+    private final By clockBottomLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockBottom\")");
+    private final By clockTopCurrentTimeLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockCurrentValue\").instance(0)");
+    private final By clockBottomCurrentTimeLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockCurrentValue\").instance(1)");
+    private final By ppBtnLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"play_pause_button\")");
+    private final By clockBtnLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"set_time_button\")");
+    private final By restartBtnLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"restart_button\")");
+    private final By topCounterLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockMovesCount\").instance(1)");
+    private final By bottomCounterLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockMovesCount\").instance(0)");
+    private final By topTimeSettingLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockTimeSetting\").instance(0)");
+    private final By bottomTimeSettingLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"clockTimeSetting\").instance(1)");
+    private final By flagLocator = AppiumBy.androidUIAutomator(
+            "new UiSelector().resourceId(\"flag_icon\")");
+
     public MainViewPOM(AppiumDriver driver) {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+        helper = new WaitHelper(driver);
     }
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockTop\")")
-    private WebElement clockTop;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockBottom\")")
-    private WebElement clockBottom;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockCurrentValue\").instance(0)")
-    private WebElement clockTopCurrentTime;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockCurrentValue\").instance(1)")
-    private WebElement clockBottomCurrentTime;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"play_pause_button\")")
-    private WebElement ppBtn;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"set_time_button\")")
-    private WebElement clockBtn;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"restart_button\")")
-    private WebElement restartBtn;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockMovesCount\").instance(1)")
-    private WebElement topCounter;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockMovesCount\").instance(0)")
-    private WebElement bottomCounter;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockTimeSetting\").instance(0)")
-    private WebElement topTimeSetting;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"clockTimeSetting\").instance(1)")
-    private WebElement bottomTimeSetting;
-
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"flag_icon\")")
-    private WebElement flag;
-
     public void tapClockTop() {
-        clockTop.click();
+        getClockTop().click();
     }
 
     public void tapBottomClock() {
-        clockBottom.click();
+        getClockBottom().click();
     }
 
     public Boolean isTopClockEnabled() {
-        return clockTop.isEnabled();
+        return getClockTop().isEnabled();
     }
 
     public Boolean isBottomClockEnabled() {
-        return clockBottom.isEnabled();
+        return getClockBottom().isEnabled();
     }
 
-    //TODO fix this usage
     public String getTopClockTime() {
-        return clockTopCurrentTime.getText();
-    }
-
-    public WebElement getTopClockTimeElement() {
-        return clockTopCurrentTime;
-    }
-
-    public WebElement getBottomClockTimeElement() {
-        return clockBottomCurrentTime;
+        return helper.waitForElement(clockTopCurrentTimeLocator, 5).getText();
     }
 
     public String getBottomClockTime() {
-        return clockBottomCurrentTime.getText();
-    }
-
-    public WebElement getPpBtn() {
-        return ppBtn;
+        return helper.waitForElement(clockBottomCurrentTimeLocator, 5).getText();
     }
 
     public Boolean isPpBtnEnabled() {
-        return ppBtn.isEnabled();
-    }
-
-    public WebElement getClockBtn() {
-        return clockBtn;
+        return getPpBtn().isEnabled();
     }
 
     public Boolean isClockBtnEnabled() {
-        return clockBtn.isEnabled();
-    }
-
-    public WebElement getRestartBtn() {
-        return restartBtn;
+        return getClockBtn().isEnabled();
     }
 
     public Boolean isRestartBtnEnabled() {
-        return restartBtn.isEnabled();
+        return getRestartBtn().isEnabled();
     }
 
     public int getTopMovesCounterValue() {
-        return parseMovesCount(topCounter.getText());
+        return parseMovesCount(helper.waitForElement(topCounterLocator, 5).getText());
     }
 
     public int getBottomMovesCounterValue() {
-        return parseMovesCount(bottomCounter.getText());
+        return parseMovesCount(helper.waitForElement(bottomCounterLocator, 5).getText());
     }
 
     public String getTopTimeSetting() {
-        return topTimeSetting.getText();
+        return helper.waitForElement(topTimeSettingLocator, 5).getText();
     }
 
     public String getBottomTimeSetting() {
-        return bottomTimeSetting.getText();
+        return helper.waitForElement(bottomTimeSettingLocator, 5).getText();
     }
+
+    public WebElement getFlag() {
+        return helper.waitForElement(flagLocator, 5);
+    }
+
+    private WebElement getClockTop() {
+        return helper.waitForElement(clockTopLocator, 5);
+    }
+
+    private WebElement getClockBottom() {
+        return helper.waitForElement(clockBottomLocator, 5);
+    }
+
+    private WebElement getPpBtn() {
+        return helper.waitForElement(ppBtnLocator, 5);
+    }
+
+    private WebElement getClockBtn() {
+        return helper.waitForElement(clockBtnLocator, 5);
+    }
+
+    private WebElement getRestartBtn() {
+        return helper.waitForElement(restartBtnLocator, 5);
+    }
+
 
     private int parseMovesCount(String unformatted) {
         int index = unformatted.indexOf(':');
         String formatted = unformatted.substring(index + 2).trim();
         return Integer.parseInt(formatted);
-    }
-
-    public WebElement getClockTop() {
-        return clockTop;
-    }
-
-    public WebElement getClockBottom() {
-        return clockBottom;
-    }
-
-    public WebElement getFlag() {
-        return flag;
     }
 }
 
---
+++ app/src/test/java/com/balbina/clockktests/pom/utility/WaitHelper.java
@@ -0,0 +1,23 @@
+package com.balbina.clockktests.pom.utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitHelper {
+
+    private final WebDriver driver;
+
+    public WaitHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement waitForElement(By locator, long seconds) {
+        return new WebDriverWait(driver, Duration.ofSeconds(seconds)).until(
+                ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
